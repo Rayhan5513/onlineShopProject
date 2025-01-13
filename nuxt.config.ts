8be2cabb85d42aa1cf14082e6cbdad39a0754903
Rayhan5513 <58135041+Rayhan5513@users.noreply.github.com>
2025-01-13 16:15:35 +0600

    Install TailwindCSS

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  css: [
+    '@/assets/css/tailwind.css', // Include the Tailwind CSS file
+  ],
+  postcss: {
+    plugins: {
+      tailwindcss: {}, // Add Tailwind CSS as a PostCSS plugin
+      autoprefixer: {}, // Add Autoprefixer as a PostCSS plugin
+    },
+  },
 })
